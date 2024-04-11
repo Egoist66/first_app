efd034d10a05804ccc1fd28f2999f12606663153
Farid Mahmudov <Razormad666@gmail.com>
2024-04-11 19:14:46 +0300

    update

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'posts';
+    protected $guarded = [];
+
+    final public function category(): BelongsTo
+    {
+
+        return $this->belongsTo(Category::class);
+    }
+
+    final public function tags(): BelongsToMany
+    {
+
+        return $this->belongsToMany(Tag::class);
+    }
+}
